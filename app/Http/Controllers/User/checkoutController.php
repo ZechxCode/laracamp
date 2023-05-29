6a0d62810679d753d2d79574b85b41d5763411fc
ZechxCode <zechxwork@gmail.com>
2023-05-29 22:11:15 +0800

    Checkout Validation

--- app/Http/Controllers/User/checkoutController.php
+++ app/Http/Controllers/User/checkoutController.php
@@ -7,6 +7,7 @@ use App\Models\Checkout;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\User\Checkout\Store;
 
 class checkoutController extends Controller
 {
@@ -48,10 +49,10 @@ class checkoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
         // return $camp;
-        // return $request->all();
+        return $request->all();
 
         //mapping request data
         $data = $request->all();
