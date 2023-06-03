7aaf052ab171cf70e36e726aa72949f7d2693c75
ZechxCode <zechxwork@gmail.com>
2023-06-03 12:21:53 +0800

    Admin Email Paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Checkout;
+use App\Mail\Checkout\Paid;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class CheckoutController extends Controller
 {
@@ -12,6 +14,8 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+        // send email to user
+        Mail::to($checkout->user->email)->send(new Paid($checkout));
         $request->session()->flash('success', "Checkout Camp {$checkout->camp->title}, INV-{$checkout->id} has been updated successfully");
         return redirect(route('admin.dashboard'));
     }
