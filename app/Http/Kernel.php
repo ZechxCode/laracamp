e3808ab369a71da38123c3caf14d290962737076
ZechxCode <zechxwork@gmail.com>
2023-11-15 00:33:34 +0700

    feat: discount management admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cekuser' => \App\Http\Middleware\UserMiddleware::class,
         'ensureUserRole' => \App\Http\Middleware\EnsureUserRole::class,
     ];
 }
